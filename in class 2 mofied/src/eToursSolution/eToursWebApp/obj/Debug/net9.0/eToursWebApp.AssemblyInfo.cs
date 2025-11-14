58a675fed7db92e95c0c741303c1ca2bdda4beb3
Daniel <d.arancibia.oyarzo@gmail.com>
2025-11-14 06:04:22 -0700

    updated ctourclass

--- in class 2 mofied/src/eToursSolution/eToursWebApp/obj/Debug/net9.0/eToursWebApp.AssemblyInfo.cs
+++ in class 2 mofied/src/eToursSolution/eToursWebApp/obj/Debug/net9.0/eToursWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eToursWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3143c95195cd619bd0062e7c2fd2a6056300173f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488ac5a55209c9eae139713e4b3c20d9d0b6e8fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("eToursWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eToursWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
