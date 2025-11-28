333283dfa0f45ed963945cca73c253287f33132c
Danielaaron1111 <d.arancibia.oyarzo@gmail.com>
2025-11-28 07:46:09 -0700

    asd

---
+++ WestWindSolution/WestWindApp/Program.cs
@@ -0,0 +1,46 @@
+
+using WestWindApp.Components;
+
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem;
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//retrieve the connection string from the appsetting.json
+//the connection string will be passed to the class library using
+//  the encapsulated extension method
+//the connection string will be registered to get access to the database
+string connectionstring = builder.Configuration.GetConnectionString("WWDB");
+
+//get access to any extended services
+//gain access to any available services that have been registered in IServiceCollections
+//the technique used in this example has the registrations encapsulated
+//  within the class library via an extension method
+//technically, you could put all the setup that is in the extension method
+//  here in this file
+builder.Services.WestWindExtensionServices(options => options.UseSqlServer(connectionstring));
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ WestWindSolution/WestWindSystem/BLL/ShipperServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class ShipperServices
+    {
+        #region setup of the context connection variable and class constructor
+        private readonly WestWindContext _context;
+
+        internal ShipperServices(WestWindContext registeredcontext)
+        {
+            _context = registeredcontext;
+        }
+        #endregion
+
+        #region Service Queries
+        //return a list of shippers
+        public List<Shipper> Shipper_GetAll()
+        {
+            IEnumerable<Shipper> info = _context.Shippers
+                                                .OrderBy(x => x.CompanyName);
+            return info.ToList();
+        }
+        #endregion
+    }
+}
